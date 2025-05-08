65b87632ca0d73c9574ea66ef4a40f9b43715569
Juane003 <j.emilio164@gmail.com>
2025-05-08 18:58:39 -0300

    test

--- app/index.tsx
+++ app/index.tsx
@@ -8,6 +8,7 @@ export default function Index() {
       <Box className="flex-1 items-center justify-center">
         <Text>Edit app/index.tsx to edit this screen.</Text>
         <Text>Hello World</Text>
+        <Box>Commit test</Box>
       </Box>
     </GluestackUIProvider>
   );
